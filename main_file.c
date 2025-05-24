6bbcd6d627d83275be23dfff5fa8a828fde454ef
ATHARVA.M <matharva357@gmail.com>
2025-05-24 11:12:29 +0530

    edit 2

--- main_file.c
+++ main_file.c
@@ -8,3 +8,21 @@
 #define MAX_TRANSACTIONS 100
 #define FILENAME "bank_data.dat"
 #define TRANSACTION_FILE "transactions.dat"
+
+typedef struct {
+    int acc_number;
+    char name[100];
+    char address[200];
+    char phone[15];
+    float balance;
+    char created_date[20];
+    int is_active;
+} Account;
+
+typedef struct {
+    int acc_number;
+    char type[20]; // "DEPOSIT", "WITHDRAWAL", "TRANSFER"
+    float amount;
+    char date[20];
+    float balance_after;
+} Transaction;
