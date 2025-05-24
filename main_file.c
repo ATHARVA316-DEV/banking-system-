9c5e55bf6fe6fef35b75f9333120704dfe9c79d3
ATHARVA.M <matharva357@gmail.com>
2025-05-24 11:10:59 +0530

    first edit

--- main_file.c
+++ main_file.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
 
+#define MAX_ACCOUNTS 1000
+#define MAX_TRANSACTIONS 100
+#define FILENAME "bank_data.dat"
+#define TRANSACTION_FILE "transactions.dat"
